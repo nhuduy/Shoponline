2133d07226befc65cf821e35c28d34a909aad3af
Duy Tran <nhuduy2008@gmail.com>
2019-07-08 19:55:05 +0700

    #5: Fix bug (not show khuyenmai index)

--- backend/khuyenmai/create.php
+++ backend/khuyenmai/create.php
@@ -7,7 +7,7 @@ require_once __DIR__.'/../../bootstrap.php';
 include_once(__DIR__.'/../../dbconnect.php');
 
 // 2. Nếu người dùng có bấm nút Đăng ký thì thực thi câu lệnh UPDATE
-if(isset($_POST['btnCapNhat'])) 
+if(isset($_POST['btnCapNhat']))
 {
     // Lấy dữ liệu người dùng hiệu chỉnh gởi từ REQUEST POST
     $km_ma = $_POST['km_ma'];
@@ -17,7 +17,14 @@ if(isset($_POST['btnCapNhat']))
     $km_denngay = $_POST['km_denngay'];
 
     // Câu lệnh INSERT
-    $sql = "INSERT INTO `khuyenmai` (km_ma, km_ten, km_noidung, km_tungay, km_denngay) VALUES ('" . $km_ma . "' ,'" . $km_ten . "', '". $km_noidung ."', '" . $km_tungay . "', '" . $km_denngay . "');";
+    $sql = "INSERT INTO `khuyenmai` (km_ten,
+                                    km_noidung,
+                                    km_tungay,
+                                    km_denngay)
+            VALUES ('" . $km_ten . "',
+                     '". $km_noidung ."',
+                     '" . $km_tungay . "',
+                     '" . $km_denngay . "');";
 
     // Thực thi INSERT
     mysqli_query($conn, $sql);
